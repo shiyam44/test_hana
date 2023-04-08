9a8f9220fd50b185c898ea9b1c8f3f437751d53c
shiyam44 <43647776+shiyam44@users.noreply.github.com>
2023-04-08 21:30:49 +0530

    Create test_sp_1.sql

---
+++ test_sp_1.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PROCEDURE YSFR.CODE_REVIEW_TEST
+(            
+       
+)
+LANGUAGE SQLSCRIPT AS v_mapval2 NVARCHAR(60)
+;
+
+
+BEGIN
+
+---This is a test procedure STG(HS2)
+--this line is deleted
+
+End
